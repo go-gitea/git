c715a1e417706e683f18a6852463ca89f8e69e41
Mura Li <mura_li@castech.com.tw>
2017-09-28 18:03:38 +0800

    Update repo_merge_test.go

--- repo_merge_test.go
+++ repo_merge_test.go
@@ -2,7 +2,6 @@ package git
 
 import (
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"testing"
 	"time"
@@ -14,7 +13,7 @@ func TestRepoMerge(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.RemoveAll(tmpdir)
+	//defer os.RemoveAll(tmpdir)
 
 	for _, x := range []struct {
 		//Input
@@ -30,27 +29,37 @@ func TestRepoMerge(t *testing.T) {
 		errorString   string
 	}{
 		{
-			repoName:      "repo-ff",
+			repoName:      "ff.git",
 			oursBranch:    "master",
-			theirsBranch:  "bye-world",
-			author:        &Signature{Email: "Alice@example.com", Name: "Alice", When: time.Date(2017, time.September, 20, 0, 0, 0, 0, time.UTC)},
-			committer:     &Signature{Email: "Alice@example.com", Name: "Alice", When: time.Date(2017, time.September, 25, 0, 0, 0, 0, time.UTC)},
-			message:       "Merge branch 'bye-world' of repo-ff",
+			theirsBranch:  "test-branch",
+			author:        &Signature{Email: "alice@example.com", Name: "Alice", When: time.Date(2005, time.April, 7, 22, 13, 13, 0, time.UTC)},
+			committer:     &Signature{Email: "alice@example.com", Name: "Alice", When: time.Date(2005, time.April, 7, 22, 13, 13, 0, time.UTC)},
+			message:       "Merge branch 'test-branch'",
 			errorString:   "",
-			mergeCommitID: "0896984c31681a77c7660bacd56de91c15389e88",
+			mergeCommitID: "57b5d2ba9bceb08e8111c676418ddcc872c50cf2",
 		},
 		{
-			repoName:      "repo-conflict",
-			oursBranch:    "bye-world",
-			theirsBranch:  "greetings",
-			author:        &Signature{Email: "Alice@example.com", Name: "Alice", When: time.Date(2017, time.September, 20, 0, 0, 0, 0, time.UTC)},
-			committer:     &Signature{Email: "Alice@example.com", Name: "Alice", When: time.Date(2017, time.September, 25, 0, 0, 0, 0, time.UTC)},
-			message:       "Merge branch 'greetings' of repo-conflict",
+			repoName:      "conflict.git",
+			oursBranch:    "test-branch1",
+			theirsBranch:  "test-branch2",
+			author:        &Signature{Email: "alice@example.com", Name: "Alice", When: time.Date(2005, time.April, 7, 22, 13, 13, 0, time.UTC)},
+			committer:     &Signature{Email: "alice@example.com", Name: "Alice", When: time.Date(2005, time.April, 7, 22, 13, 13, 0, time.UTC)},
+			message:       "Merge branch 'test-branch2'",
 			errorString:   "exit status 128 - ERROR: content conflict in README.md\nfatal: merge program failed\n",
 			mergeCommitID: "",
 		},
+		{
+			repoName:      "adding.git",
+			oursBranch:    "master",
+			theirsBranch:  "test-branch",
+			author:        &Signature{Email: "alice@example.com", Name: "Alice", When: time.Date(2005, time.April, 7, 22, 13, 13, 0, time.UTC)},
+			committer:     &Signature{Email: "alice@example.com", Name: "Alice", When: time.Date(2005, time.April, 7, 22, 13, 13, 0, time.UTC)},
+			message:       "Merge branch 'test-branch'",
+			errorString:   "",
+			mergeCommitID: "7f19d72c899e7d8fd2d46724bc95dfd4904704b4",
+		},
 	} {
-		from := filepath.Join("testdata", x.repoName)
+		from := filepath.Join("testdata/generated", x.repoName)
 		to := filepath.Join(tmpdir, x.repoName)
 		if err := Clone(from, to, CloneRepoOptions{
 			Timeout: 5 * time.Second,
@@ -69,7 +78,7 @@ func TestRepoMerge(t *testing.T) {
 
 		if err := repo.MergeBranch(x.committer, x.oursBranch, x.theirsBranch, x.message); err != nil {
 			if err.Error() != x.errorString {
-				t.Fatal(err)
+				t.Fatalf("'%s': %v", x.repoName, err)
 			}
 		} else {
 			oursID, err := repo.GetBranchCommitID(x.oursBranch)
@@ -78,7 +87,7 @@ func TestRepoMerge(t *testing.T) {
 			}
 
 			if oursID != x.mergeCommitID {
-				t.Fatalf("Not matched. Actual:%s Expected:%s", oursID, x.mergeCommitID)
+				t.Fatalf("'%s': Not matched. Actual:%s Expected:%s", x.repoName, oursID, x.mergeCommitID)
 			}
 		}
 	}
